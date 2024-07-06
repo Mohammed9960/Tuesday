be547d515897e40d1606c665a5bdc2eb3f226bfc
Mohammed <mohammed.swe97@gmail.com>
2024-07-06 17:06:07 +0300

    adding security

---
+++ CrudWithChangelog/src/main/java/com/crud/crudwithchangelog/MyUser.java
@@ -0,0 +1,25 @@
+package com.crud.crudwithchangelog;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyUser  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String role;
+
+
+}
---
+++ CrudWithChangelog/src/main/java/com/crud/crudwithchangelog/config/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package com.crud.crudwithchangelog.config;
+
+import com.crud.crudwithchangelog.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+        private final UserService userService;
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+        @Bean
+        SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+            return httpSecurity
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(registry-> {
+                        registry.requestMatchers("/register/**","/authenticated").permitAll();
+                        registry.anyRequest().authenticated();
+                    })
+                    .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                    .build();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService() {
+            return userService;
+        }
+
+        @Bean
+        public AuthenticationProvider authenticationProvider() {
+            DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+            daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+            daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+            return daoAuthenticationProvider;
+
+        }
+        @Bean
+        public AuthenticationManager authenticationManager(){
+            return new ProviderManager(authenticationProvider());
+        }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+
+    }
+
+
